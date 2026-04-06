61a3bea223b17123ee8353e55f78853e63a4589f
Arzelir <365yaseen@gmail.com>
2026-04-06 16:54:23 -0400

    decreased word list size

--- words.py
+++ words.py
@@ -3,7 +3,7 @@ import random
 from wordfreq import top_n_list
 
 def get_word_list():
-    words = top_n_list("en", 50000)
+    words = top_n_list("en", 10000)
     return [w for w in words if len(w) == 5 and w.isalpha()]
 
 WORD_LIST = get_word_list()
