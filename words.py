f5619a7aa982f1255126b3c34b6f3a371cf6e89c
khalidb21 <142428515+khalidb21@users.noreply.github.com>
2026-04-07 15:46:27 -0400

    Add comment to words.py

--- words.py
+++ words.py
@@ -2,6 +2,7 @@
 import random
 from wordfreq import top_n_list
 
+# Keep words that are only 5 letters long and contain only alphabetic characters
 def get_word_list():
     words = top_n_list("en", 10000)
     return [w for w in words if len(w) == 5 and w.isalpha()]
